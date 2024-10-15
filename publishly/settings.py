f517cb1c5fcc3a4f7dd34d096b3f8626c156a5e4
yibrino <yibrahmehari128@gmail.com>
2024-10-16 00:43:36 +0200

    Cors updated

--- publishly/settings.py
+++ publishly/settings.py
@@ -31,9 +31,10 @@ SECRET_KEY = env("SECRET_KEY")
 DEBUG = True
 # DEBUG = env("DEBUG")
 ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
+
 CORS_ALLOWED_ORIGINS = [
         "http://localhost:3000",
-        "https://publishly-46a2edd7f6b7.herokuapp.com",
+        "https://publishlyfrontend-fb2f951edda2.herokuapp.com",
     
 ]
 
